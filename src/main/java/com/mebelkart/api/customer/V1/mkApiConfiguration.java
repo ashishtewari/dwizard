f7ce2a92a6e85b8a0a7760acdbd2ef37392cb887
nikhilreddy93 <nikhilreddy2493@gmail.com>
2016-03-10 18:44:14 +0530

    Initial Commit

---
+++ src/main/java/com/mebelkart/api/customer/V1/mkApiConfiguration.java
@@ -0,0 +1,10 @@
+package com.mebelkart.api.customer.V1;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class mkApiConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
