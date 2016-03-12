68f5bb09245853122bda00d80346135be365a5bf
Vin_code <vinit.payal@mebelkart.com>
2016-03-12 19:50:29 +0530

    commit without gitignore

---
+++ front-api/src/main/java/com/mebelkart/api/customer/v1/core/CustomerApiAuthentication.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package com.mebelkart.api.customer.v1.core;
+
+import java.util.List;
+
+import com.mebelkart.api.customer.v1.dao.CustomerAuthenticationDAO;
+
+/**
+ * @author Nikky-Akky
+ *
+ */
+public class CustomerApiAuthentication {
+
+	/*
+	 * authenticating the user
+	 */
+	CustomerAuthenticationDAO authentication;
+	private String key;
+
+	public CustomerApiAuthentication(CustomerAuthenticationDAO authentication, String key) {
+		this.authentication = authentication;
+		this.key = key;
+	}
+
+	/**
+	 * It validates the apikey given by the user. 
+	 */
+	public boolean isAuthKeyValid() {
+		System.out.println("entered in auth key valid");
+		List<CustomerAuthenticatonWrapper> list = this.authentication.isCustomerValid(key.trim());
+		System.out.println("key = " + key);
+		System.out.println("size = "+list.size());
+		if (list.size() > 0 && key != "") {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * It checks whether the customer has permission to access the requests like GET,POST,PUT and DELETE.
+	 * and it returns boolean result.
+	 */
+	public boolean isCustomerPermitted(int authValue){
+		List<CustomerAuthenticatonWrapper> list = this.authentication.isCustomerValid(key.trim());
+		System.out.println("auth value = " + list.get(0).getIsHavingGetPermission());
+		if(list.get(0).getIsHavingGetPermission() == authValue){
+			return true;
+		}
+		return false;
+		
+	}
+	
+}
