7347ae8a15a663ba0481acc748d6275f5904740a
Diveshreddy <diveshreddy@gmail.com>
2016-05-18 15:19:50 +0530

    Update response structure of attributeGroups in products

---
+++ front-api/src/main/java/com/mebelkart/api/product/v1/core/AttributeGroupsOuterPOJO.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.mebelkart.api.product.v1.core;
+
+/**
+ * @author Tinku
+ *
+ */
+public class AttributeGroupsOuterPOJO {
+	
+	private Object groups;
+	private Object attributeMappings;
+
+	/**
+	 * Parameterized Constructer
+	 */
+	public AttributeGroupsOuterPOJO(Object groups,Object attributeMappings) {
+		this.setGroups(groups);
+		this.setAttributeMappings(attributeMappings);
+	}
+
+	public Object getGroups() {
+		return groups;
+	}
+
+	public void setGroups(Object groups) {
+		this.groups = groups;
+	}
+
+	public Object getAttributeMappings() {
+		return attributeMappings;
+	}
+
+	public void setAttributeMappings(Object attributeMappings) {
+		this.attributeMappings = attributeMappings;
+	}
+
+}
